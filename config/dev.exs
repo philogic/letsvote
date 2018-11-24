833f03143eaba6e7f25c319b0cebfd7b2f0bc021
Jakub Oblizajek <jakub.oblizajek@gmail.com>
2018-11-24 15:33:04 +0100

    Add plug_cowboy as a dependency (necessary to run phoenix server), change database configuration, and port number

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :letsvote, LetsvoteWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 3000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :letsvote, Letsvote.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "xcodeclub",
+  password: "",
   database: "letsvote_dev",
   hostname: "localhost",
   pool_size: 10
