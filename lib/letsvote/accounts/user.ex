c2fab70789f966ac44e3446c21dd7a9bdb44ceec
Jakub Oblizajek <jakub.oblizajek@gmail.com>
2018-11-26 14:12:02 +0100

    Create user schema with changeset method.

---
+++ lib/letsvote/accounts/user.ex
@@ -0,0 +1,19 @@
+defmodule Letsvote.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Letsvote.Accounts.User
+
+  schema "users" do
+    field(:username, :string)
+    field(:email, :string)
+    field(:active, :boolean, default: true)
+    field(:encrypted_password, :string)
+    timestamps()
+  end
+
+  def changeset(%User{} = user, attrs) do
+    user
+    |> cast(attrs, [:username, :email, :active])
+    |> validate_required([:username, :email, :active])
+  end
+end
