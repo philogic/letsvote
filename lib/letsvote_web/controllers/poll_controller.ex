e8996217b9964eef96f1cd99305989033f58f353
Jakub Oblizajek <jakub.oblizajek@gmail.com>
2018-11-27 03:24:27 +0100

    Implement new user feature at a controller level. Template is still a stub. Change the default layout to custom.html. Change the layout name from custom to app.html.eex (The original generatedd layout stays as a backup.

--- lib/letsvote_web/controllers/poll_controller.ex
+++ lib/letsvote_web/controllers/poll_controller.ex
@@ -1,23 +1,20 @@
 defmodule LetsvoteWeb.PollController do
   use LetsvoteWeb, :controller
+  alias Letsvote.Votes
 
   def index(conn, _params) do
-    polls = Letsvote.Votes.list_polls()
-    conn
-    |> put_layout(:custom)
-    |> render("index.html", polls: polls)
+    polls = Votes.list_polls()
+    render(conn, "index.html", polls: polls)
   end
 
   def new(conn, _params) do
-    poll = Letsvote.Votes.new_poll()
-    conn
-    |> put_layout(:custom)
-    |> render("new.html", poll: poll)
+    poll = Votes.new_poll()
+    render(conn, "new.html", poll: poll)
   end
 
   def create(conn, %{"poll" => poll_params, "options" => options}) do
     split_options = String.split(options, ",")
-    with {:ok, poll} <- Letsvote.Votes.create_polls_and_options(poll_params, split_options) do
+    with {:ok, poll} <- Votes.create_polls_and_options(poll_params, split_options) do
       conn
       |> put_flash(:info, "Poll added!")
       |> redirect(to: poll_path(conn, :index))
--- lib/letsvote_web/controllers/user_controller.ex
+++ lib/letsvote_web/controllers/user_controller.ex
@@ -1,10 +1,15 @@
 defmodule LetsvoteWeb.UserController do
   use LetsvoteWeb, :controller
+  alias Letsvote.Accounts
+
+  def new(conn, _) do
+    user = Accounts.new_user()
+    render(conn, "new.html", user: user)
+  end
+
 
   def show(conn, %{"id" => id}) do
-    user = Letsvote.Accounts.get_user(id)
-    conn
-    |> put_layout(:custom)
-    |> render("show.html", user: user)
+    user = Accounts.get_user(id)
+    render(conn, "show.html", user: user)
   end
 end
