81aa88df53c8e4138637ec866221fbd4ff6ff184
Jakub Oblizajek <jakub.oblizajek@gmail.com>
2018-12-19 17:39:13 +0100

    Handle real-time voting (i.e. without having to reload the page each time)

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -1,5 +1,26 @@
 import { Socket } from 'phoenix';
 
+const pushVote = (el, channel) => {
+    channel
+        .push('vote', {option_id: el.getAttribute('data-option-id')})
+        .receive('ok', res => console.log('You voted!'))
+        .receive('error', res => console.log('Failed', res))
+        .on('new_vote', ({option_id, votes}) => {
+            document.getElementById('vote-count-' + option_id).innerHTML = votes
+        })
+}
+
+const onJoinChannel = (res, channel) => {
+    document.querySelectorAll('.vote-button-manual').forEach(el => {
+        el.addEventListener('click', event => {
+            event.preventDefault();
+            pushVote(el, channel);
+            console.log('Do something!')
+        })
+    });
+    console.log('Joined channel: ', res)
+};
+
 const socket = new Socket("/socket");
 socket.connect();
 
@@ -7,7 +28,7 @@ if (document.getElementById('enable-polls-channel')) {
     const channel = socket.channel('polls:main');
     channel
         .join()
-        .receive('ok', res => console.log('Joined channel:', res))
+        .receive('ok', res => {onJoinChannel(res, channel)})
         .receive('error', res => console.log('Failed to join channel:', res));
     document.getElementById("polls-ping").addEventListener("click", () => {
         channel
--- lib/letsvote_web/channels/polls_channel.ex
+++ lib/letsvote_web/channels/polls_channel.ex
@@ -5,8 +5,14 @@ defmodule LetsvoteWeb.PollsChannel do
     {:ok, socket}
   end
 
-  def handle_in("hello", _payload, socket) do
-    broadcast socket, "world", %{message: "world"}
-    {:reply, {:ok, %{message: "world"}}, socket}
+
+  def handle_in("vote", %{"option_id" => option_id}, socket) do
+    with {:ok, option} <- Letsvote.Votes.choose_option(option_id) do
+      broadcast socket, "new_vote", %{"option_id" => option.id, "votes" => option.votes}
+      {:reply, {:ok, %{"option_id" => option_id, "votes" => option.votes}}, socket}
+    else
+      {:error, _} ->
+        {:reply, {:error,%{message: "Failed to vote"}}}
+    end
   end
 end
